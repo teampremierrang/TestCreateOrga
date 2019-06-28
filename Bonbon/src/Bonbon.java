1d566047c3d7035b11fdea22a2b5c0973cc9034b
ib <ib@WINIB-ELSQQ20U0>
2019-06-28 09:15:26 +0200

    projet bonbon

---
+++ Bonbon/src/Bonbon.java
@@ -0,0 +1,17 @@
+
+public class Bonbon {
+	
+	private String marque;
+	private String name;
+	/**
+	 * @param marque
+	 * @param name
+	 */
+	public Bonbon(String marque, String name) {
+	
+		this.marque = marque;
+		this.name = name;
+	}
+	
+
+}
