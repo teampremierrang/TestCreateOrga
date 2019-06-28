6b62a2ce8766e1ce5b9aa54034462b9d908b8b2d
ib <ib@WINIB-ELSQQ20U0>
2019-06-28 09:49:01 +0200

    ajout atribut prix : double

--- Bonbon/src/Bonbon.java
+++ Bonbon/src/Bonbon.java
@@ -3,19 +3,24 @@ public class Bonbon {
 	
 	private String marque;
 	private String name;
+	private double prix;
+
 	/**
 	 * @param marque
 	 * @param name
+	 * @param prix
 	 */
-	public Bonbon(String marque, String name) {
+	public Bonbon(String marque, String name, double prix) {
 	
 		this.marque = marque;
 		this.name = name;
+		this.prix = prix;
 	}
+	
 //	Test SANSAN
 	
 	// I m the king of the world
 	//testBranche 2
 	
-
+	
 }
